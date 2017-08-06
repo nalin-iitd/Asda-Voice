5015bc4af021241a2dd1c7780bad7d2b7b2f7b38
vn09zjz <NKumar@walmartlabs.com>
2017-08-06 13:54:54 -0700

    initial changes

--- index.js
+++ index.js
@@ -7,49 +7,34 @@ var http = require('http');
 const restService = express();
 restService.use(bodyParser.json());
 
-restService.get('/hook', function (req, res) {
+restService.post('/hook', function (req, res) {
 
     console.log('hook request');
 
     try {
-        // var speech = 'empty speech';
-
-        // if (req.body) {
-        //     var requestBody = req.body;
-
-        //     if (requestBody.result) {
-        //         speech = '';
-
-        //         if (requestBody.result.itemName) {
-        //             speech += requestBody.result.itemName.speech;
-        //             speech += ' ';
-        //         }
-
-        //         if (requestBody.result.action) {
-        //             speech += 'action: ' + requestBody.result.action;
-        //         }
-        //     }
-        // }
-
         var appId = '0cc5117b';
         var appKey = '7fd6ee57ca3497bc04bf70a71a714a97';
-        //var requestURL = 'http://api.yummly.com/v1/api/recipes?_app_id=' + appId + '&_app_key=' + appKey + '&q=onion+soup';
-        //var requestURL = 'https://test-es.edamam.com/search?q=' + 'chicken';
-        var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.itemName ? req.body.result.parameters.itemName : "Seems like some problem. Speak again."
+        var speech = req.body.result && req.body.result.parameters && req.body.result.parameters.itemName ? req.body.result.parameters.itemName : "";
 
         var recipeSearchTerm = '';
-        var speechArr = speech.split(' ');
+        var requestURL = '';
 
-        for (var i = 0; i < speechArr.length; i++) {
-            if (i < speechArr.length - 1) {
-                recipeSearchTerm += recipeSearchTerm + speechArr + "+";
-            } else if (i == speechArr.length - 1) {
-                recipeSearchTerm += recipeSearchTerm + speechArr;
+        if (speech) {
+            var speechArr = speech.split(' ');
+            for (var i = 0; i < speechArr.length; i++) {
+                if (i < speechArr.length - 1) {
+                    recipeSearchTerm += recipeSearchTerm + speechArr + "+";
+                } else if (i == speechArr.length - 1) {
+                    recipeSearchTerm += recipeSearchTerm + speechArr;
+                }
             }
-        }
 
-        var requestURL = 'http://api.yummly.com/v1/api/recipes?_app_id=' + appId + '&_app_key=' + appKey + '&q=onion+soup';
-        //var requestURL = 'https://test-es.edamam.com/search?q=' + 'chicken';
+            requestURL = 'http://api.yummly.com/v1/api/recipes?_app_id=' + appId + '&_app_key=' + appKey + '&q=' + recipeSearchTerm;
+        } else {
+            // nothing coming from the speech 
+            speech = "Please try again!!!";
+            requestURL = 'http://api.yummly.com/v1/api/recipes?_app_id=' + appId + '&_app_key=' + appKey + '&q=onion+soup';
+        }
 
         console.log(requestURL);
         console.log(speech);
@@ -65,11 +50,9 @@ restService.get('/hook', function (req, res) {
             });
             // After the response is completed, parse it and log it to the console
             response.on('end', function () {
-                console.log(requestURL);
                 var parsed = JSON.parse(body);
                 str = parsed;
-                console.log(str.totalMatchCount);
-                //handleResult(str.totalMatchCount);
+               
                 return res.json({
                     speech: speech,
                     displayText: speech,
